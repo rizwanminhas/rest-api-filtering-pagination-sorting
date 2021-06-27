0104fd9630e3d8002a2b4c74f8ac06164dd952d7
Rizwan Minhas <rizjav@gmail.com>
2021-06-27 13:14:16 -0400

    Adding page/offset based pagination implementation

--- index.js
+++ index.js
@@ -10,8 +10,12 @@ const pool = new Pool({
 })
 const port = process.env.PORT || 3000
 
-app.get('/*', (req, res) => {
-    pool.query('SELECT * from products', (err, dbres) => {
+app.get('/page', (req, res) => {
+    const limit = req.query.limit || 10
+    const offset = req.query.offset || 0
+    const query = 'SELECT * FROM products ORDER BY product_id LIMIT $1 OFFSET $2'
+    const values = [limit, offset]
+    pool.query(query, values, (err, dbres) => {
         if (err) {
             throw err
         }
